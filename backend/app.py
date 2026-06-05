63f5f09aeb0764f27dee6d2abd3c5b8d9a6ec65d
Vivek Velugoti <vivekvelugoti070705@gmail.com>
2026-06-05 11:38:42 +0530

    added gunicorn

--- backend/app.py
+++ backend/app.py
@@ -181,4 +181,4 @@ def predict():
 
 if __name__ == '__main__':
     # Run the server locally on port 5000
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
